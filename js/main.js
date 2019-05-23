e981d2ac11c5fbb073e9f17a6b2860161cc5c04f
wxq393 <wxq393@wxq393deMacBook-Pro.local>
2019-05-23 21:21:21 +0800

    进行了动画调速

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 !function(){
+  var duration = 50
+  $('.actions').on('click','button',function(e){
+    let $button = $(e.currentTarget)
+    let speed = $button.attr('data-speed')
+    console.log(speed)
+    $button.addClass('active')
+      .siblings('.active').removeClass('active')
+    switch(speed) {
+      case 'slow':
+        duration = 100
+        break
+      case 'normal':
+        duration = 50
+        break
+      case 'fast':
+        duration = 10
+        break
+    }
+  })
   function writeCode(prefix, code, fn){
      let container = document.querySelector('#code')
      let styleTag = document.querySelector('#styleTag')
      let n = 0
-     let id = setInterval(() =>{
+     let id 
+     id = setTimeout(function run(){
         n += 1
         container.innerHTML = code.substring(0,n)
         styleTag.innerHTML = code.substring(0,n)
         container.scrollTop = container.scrollHeight
-        if(n >= code.length) {
-            window.clearInterval(id)
-            fn && fn.call()
+        if(n < code.length) {
+          id = setTimeout(run, duration)
+        }else {
+          fn && fn.call()
         }
       },20)
   }
@@ -188,14 +209,6 @@
 */
 `
   writeCode('',code)
-
-  $('.actions').on('click','button',function(e){
-      let $button = $(e.currentTarget)
-      let speed = $button.attr('data-speed')
-      console.log(speed)
-      $button.addClass('active')
-        .siblings('.active').removeClass('active')
-  })
 }.call()
 
 
